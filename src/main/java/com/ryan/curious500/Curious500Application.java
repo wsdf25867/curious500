717be74326168622b310eb3d88c1d4390049c871
ryan <wsdf25867@gmail.com>
2023-01-21 21:56:47 +0900

    Initial commit

---
+++ src/main/java/com/ryan/curious500/Curious500Application.java
@@ -0,0 +1,13 @@
+package com.ryan.curious500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curious500Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curious500Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryan/curious500/Curious500ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.curious500;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curious500ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
